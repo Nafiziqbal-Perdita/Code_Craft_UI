3d0f807c77e9b31b4c583f95b663af6b8f4dee20
Nafiz iqbal <nafiziqbal.mo@gmail.com>
2025-07-18 12:20:10 +0600

    Deployed in the server

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:3001"
+// export const BACKEND_URL = "http://localhost:3001"//when it runs locally
+export const BACKEND_URL = "https://code-craft-server-n48m.onrender.com"//when it runs in server
